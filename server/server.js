8d1f250c0732867214ceb4956beb0fa9ba9b30c2
garretthilberling <ghilberling@knights.ucf.edu>
2022-05-15 18:01:26 -0400

    set up client side react front end. using concurrently to run both servers simultaneously

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
@@ -24,6 +25,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
 }
+
+// Serve up static assets
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
